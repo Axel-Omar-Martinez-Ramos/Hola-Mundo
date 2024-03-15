427c50d9dbeb8676bd6f1312e0e28d4606f4c4f6
Axel-Omar-Martinez-Ramos <159666935+Axel-Omar-Martinez-Ramos@users.noreply.github.com>
2024-03-14 23:48:14 -0600

    Version 5 (corregido)

--- memoria.cpp
+++ memoria.cpp
@@ -8,9 +8,12 @@ private:
 public:
 Personaje(){}
 ~Personaje() {}
+void Hablar(){
+    cout<<"Hola enanos"<<endl;
+}
 };
 
-class Duende
+class Duende : public Personaje
 {
 private:
 int salud;
@@ -20,32 +23,24 @@ public:
 ~ Duende() {}
 };
 
-class Enano : public Personaje{
-
-
-};
-class Enano
+class Enano : public Personaje
 {
 private:
-    float altura;
-    int edad;
-    int vida;
-    void Hablar()
+     int vida;
+     float altura;
+     int edad;
 public:
- Enano() {}
-     
- ~Enano() {}
-void inicializar(){
-    this->altura = 2;
-    this->vida = 10;
-    this->edad = 1000;
-}
-void comer(){
-    this->vida+=1;
-
-}
+     Enano(){}
+     ~Enano(){}
+     void inicializar(){
+        this->altura = 2;
+        this->edad = 150;
+        this->vida = 10;
+     }
+     void Comer(){
+        this->vida +=1;
+     }
 };
-
 
 int main(int argc, char const *argv[]){
 
@@ -53,25 +48,25 @@ int main(int argc, char const *argv[]){
     bool b=true;
     float f=0.1;
     double d=0.1;
-    char c='i';
+    char c='a';
     Enano e;
 
-
-    cout<<"Tamaño tipos de datos masivos:"<<endl;
+    cout<<endl;
+    cout<<"Tamaño tipos de datos nativos:"<<endl;
     cout<<"Tamaño int: " <<sizeof(int)<<"bytes"<<endl;
     cout<<"Tamaño bool: " <<sizeof(bool)<<"bytes"<<endl;
     cout<<"Tamaño float: " <<sizeof(float)<<"bytes"<<endl;
     cout<<"Tamaño double: " <<sizeof(double)<<"bytes"<<endl;
-    cout<<"Tamaño char: " <<sizeof(char)<<static_cast<void*>(&c)<<endl;
+    cout<<"Tamaño char: " <<sizeof(char)<<endl;
     cout<<"Tamaño Enano: " <<sizeof(Enano)<<"bytes"<<endl;
 
     cout<<endl;
-    cout<<"Memoria ºestaica en C"<<endl;
-    cout<<"Direccion de int:"<<&a <<endl;
+    cout<<"Memoria Estaica en C:"<<endl;
+    cout<<"Direccion de int:"<<&i<<endl;
     cout<<"Direccion de bool:"<<&b <<endl;
     cout<<"Direccion de float"<<&f<<endl;
     cout<<"Direccion de double:"<<&d<<endl;
-    cout<<"Direccion de char:"<<&c <<endl;
+    cout<<"Direccion de char:"<<static_cast<void*>(&c)<<endl;
     cout<<"Direccion de Enano:"<<&e<<endl;
 
     cout<<endl;
@@ -80,7 +75,7 @@ int main(int argc, char const *argv[]){
     cout<<"Direccion dinamica int"<<malloc(sizeof(int))<<endl;
 
     //Casteo de punteros en C
-    Enano* direccion =(Enano*) malloc(sizeof(int));
+    Enano* direccion =(Enano*) malloc(sizeof(Enano));
     direccion->inicializar();
 
     //Punteros en C++
@@ -104,11 +99,11 @@ int main(int argc, char const *argv[]){
     Duende duendes(10);
 
     cout<<endl;
-    cout<<&duendes(0)<<endl;
-    cout<<&duendes(1)<<endl;
-    cout<<&duendes(2)<<endl;
-    cout<<&duendes(3)<<endl;
-    cout<<&duendes(4)<<endl;
+    cout<<&duendes[0]<<endl;
+    cout<<&duendes[1]<<endl;
+    cout<<&duendes[2]<<endl;
+    cout<<&duendes[3]<<endl;
+    cout<<&duendes[4]<<endl;
 
     //Conversion a direccion
     cout<<endl;
